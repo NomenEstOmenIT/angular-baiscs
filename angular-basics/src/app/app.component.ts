48f81c954cce549185c8fbd52575f715f66d21db
nomenestomenit <romanovrodionv@gmail.com>
2019-11-04 18:27:11 +0200

    folder 3  lesson 3

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title:string
+  text: string
+  id?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basics';
+  posts:Post[]=[
+    {title: "Want learn Angular components", text:"I steal leatning componets",id: 1},
+    {title: "Next block", text:"Will be about directives and pipes",id: 2}
+  ]
 }
