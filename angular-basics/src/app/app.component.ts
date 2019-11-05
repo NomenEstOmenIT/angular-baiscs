d9bb4f0866a772a534ed14bf3bd4a3164d9a06b4
nomenestomenit <romanovrodionv@gmail.com>
2019-11-05 19:05:55 +0200

    reset

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 export interface Post {
-  title:string
-  text: string
-  id?: number
+  title: string;
+  text: string;
+  id?: number;
 }
 
 @Component({
@@ -12,8 +12,13 @@ export interface Post {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  posts:Post[]=[
-    {title: "Want learn Angular components", text:"I steal leatning componets",id: 1},
-    {title: "Next block", text:"Will be about directives and pipes",id: 2}
-  ]
+  posts: Post[] = [
+    { title: 'Want learn Angular components', text: 'I steal leatning componets', id: 1 },
+    { title: 'Next block', text: 'Will be about directives and pipes', id: 2 }
+  ];
+
+  updatePosts(post: Post) {
+    this.posts.unshift(post);
+    console.log('Post', post);
+  }
 }
--- angular-basics/src/app/app.module.ts
+++ angular-basics/src/app/app.module.ts
@@ -1,22 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { PostFormComponent } from './post-form/post-form.component';
 import { PostComponent } from './post/post.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    PostFormComponent,
-    PostComponent,
-
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, PostFormComponent, PostComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
