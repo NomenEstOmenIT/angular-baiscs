10d31501fcf884d8265efe89376dfa9e706c8f7c
nomenestomenit <romanovrodionv@gmail.com>
2019-11-27 13:57:12 +0200

    trying to write modal component

---
+++ angular-forms/src/app/my.validators.ts
@@ -0,0 +1,22 @@
+import { FormControl } from "@angular/forms";
+import { Observable } from "rxjs";
+import { resolve } from 'url';
+
+export class MyValidators {
+  static restrictedEmails(control: FormControl): { [key: string]: boolean } {
+    if (["v@mail.ru", "test@mail.ru"].includes(control.value)) {
+      return { restrictedEmail: true };
+    }
+    return null;
+  }
+
+  static uniqEmail(control: FormControl): Promise<any> | Observable<any> {
+    return new Promise(resolve=>{
+        setTimeout(()=>{
+            if(control.value === 'async@mail.ru'){
+                resolve*=
+            }
+        })
+    });
+  }
+}
